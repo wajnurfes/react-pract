d52d0a968beb0e506efb3d94a2b3d486bada7c58
Zach Campbell <beazlebubbles@gmail.com>
2020-11-04 22:11:29 -0700

    Add a variable and method to toggle

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Transdermal celebration caused a slight mutation in the rift.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Where is the mutation that once told me it was safe?
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.toggleState = false;
+  }
+
+  toggle = () => {
+    this.toggleState = !this.toggleState
+  }
+
+
+  render() { 
+    return(<div className="App"> 
+    <p onClick={this.toggle}>Cick on me!</p>
+    </div>);
+  };
 }
 
 export default App;
