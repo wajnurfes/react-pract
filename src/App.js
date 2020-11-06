7e9db398a2c5e67c51a93c77d1768fbbc03ccc9a
Zach Campbell <beazlebubbles@gmail.com>
2020-11-06 13:15:28 -0700

    Format with Prettier

--- src/App.js
+++ src/App.js
@@ -2,43 +2,42 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
-
   constructor(props) {
     super(props);
     this.state = {
       toggled: false,
-      sum: 0
-    }
+      sum: 0,
+    };
   }
 
   toggle = () => {
     this.setState({
-      toggled: !this.state.toggled
+      toggled: !this.state.toggled,
     });
-  }
+  };
 
   add = () => {
     this.setState({
-      sum: this.state.sum + 1
+      sum: this.state.sum + 1,
     });
-  }
+  };
 
-
-  render() { 
-    return(
-      <div className="App">
-        <div className="ButtonContainer">
+  render() {
+    return (
+      <div className='App'>
+        <div className='ButtonContainer'>
           {(() => {
-            if (this.state.toggled) return <button onClick={this.toggle}>Don't click on me.</button>
-            else return <button onClick={this.toggle}>Click on me!</button>
+            if (this.state.toggled)
+              return <button onClick={this.toggle}>Don't click on me.</button>;
+            else return <button onClick={this.toggle}>Click on me!</button>;
           })()}
         </div>
-        <div className="ButtonContainer">
+        <div className='ButtonContainer'>
           <button onClick={this.add}>{this.state.sum}</button>
-          </div>
+        </div>
       </div>
     );
-  };
+  }
 }
 
 export default App;
